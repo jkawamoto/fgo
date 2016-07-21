afb6ceb82fb93f010cbed1235f8dde3d0629b588
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-07-21 15:13:03 -0400

    Add config.go to maintain Config object.

--- command/prepare.go
+++ command/prepare.go
@@ -2,48 +2,18 @@ package command
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 
-	"github.com/naoina/toml"
 	"github.com/ttacon/chalk"
 	"github.com/urfave/cli"
 )
 
 const (
-	// ConfigFile defines configuration file name.
-	ConfigFile = ".fgo"
 	// DefaultPackageDir defines default package directory.
 	DefaultPackageDir = "pkg"
 	// DefaultHomebrewDir defines default homebrew formula directory.
 	DefaultHomebrewDir = "homebrew"
 )
-
-// Config defines a configuration structure.
-type Config struct {
-	// Directory to store built packages.
-	Package string
-	// Directory to store homwbrew formula.
-	Homebrew string
-}
-
-// Load loads configurations from a given path.
-func (c *Config) Load(path string) (err error) {
-
-	fp, err := os.Open(path)
-	if err != nil {
-		return
-	}
-	defer fp.Close()
-
-	buf, err := ioutil.ReadAll(fp)
-	if err != nil {
-		return
-	}
-
-	return toml.Unmarshal(buf, &c)
-
-}
 
 // Prepare checkes configuration file and loads it if exists.
 // Otherwise, set default values. In both cases, if optional flags are given,
