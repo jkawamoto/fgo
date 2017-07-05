a818e96e3b9c54d08e9511e0a12047f23945867d
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-07-05 18:00:05 -0400

    Checking OS type in brew formula.

--- assets/formula.rb
+++ assets/formula.rb
@@ -1,14 +1,37 @@
+require 'rbconfig'
 class {{.Package | Title}} < Formula
   desc ""
   homepage "https://github.com/{{.UserName}}/{{.Package}}"
   version "{{"{{.Version}}"}}"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/{{.UserName}}/{{.Package}}/releases/download/v{{"{{.Version}}"}}/{{"{{.Mac64.FileName}}"}}"
-    sha256 "{{"{{.Mac64.Hash}}"}}"
+    case RbConfig::CONFIG['host_os']
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      :windows
+    when /darwin|mac os/
+      url "https://github.com/{{.UserName}}/{{.Package}}/releases/download/v{{"{{.Version}}"}}/{{"{{.Mac64.FileName}}"}}"
+      sha256 "{{"{{.Mac64.Hash}}"}}"
+    when /linux/
+      :linux
+    when /solaris|bsd/
+      :unix
+    else
+      :unknown
+    end
   else
-    url "https://github.com/{{.UserName}}/{{.Package}}/releases/download/v{{"{{.Version}}"}}/{{"{{.Mac386.FileName}}"}}"
-    sha256 "{{"{{.Mac386.Hash}}"}}"
+    case RbConfig::CONFIG['host_os']
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      :windows
+    when /darwin|mac os/
+      url "https://github.com/{{.UserName}}/{{.Package}}/releases/download/v{{"{{.Version}}"}}/{{"{{.Mac386.FileName}}"}}"
+      sha256 "{{"{{.Mac386.Hash}}"}}"
+    when /linux/
+      :linux
+    when /solaris|bsd/
+      :unix
+    else
+      :unknown
+    end
   end
 
   def install
